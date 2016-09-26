29e4810faa99139a784587a7325adac5e37cf986
chengjiwang <chengji3728@gmail.com>
2016-09-26 10:30:54 +0800

    use callback

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 $(function(){
+	
+	$(window).scroll(function(){
+		var scrollValue = $(this).scrollTop(),
+			windowHeight = $(window).height()*0.5;
+		if(window.innerWidth > 992){
+			if (scrollValue > windowHeight) {
+				$('#up').fadeIn();
+			} else {
+				$('#up').fadeOut();
+			}
+		}
+	});
+
+    $('#up').click(function(){
+		$('html,body').animate({scrollTop:0}, 1000);
+	});
+
     function checkLocation(location) {
 		if (location === "KaohsiungCulturalCenter") {
 			return "高雄市文化中心";
@@ -19,72 +36,47 @@ $(function(){
     		return "特定對象";
     	}
     }
-    function listData(artData){
-    	var artLength = artData.length;
-		if(artLength!= 0){
-			for (i = 0; i < artLength; i++){
-				var name = artData[i].PRGNAME,
-					performer = artData[i].PRGACT,
-					date = artData[i].PRGDATE,
-					startTime = artData[i].PRGSTIME,
-					endTime = artData[i].PRGETIME,
-					place = artData[i].PRGPLACE,
-					description =artData[i].ITEMDESC,
-					img = artData[i].IMAGE1,											 						      						 				      
-					art = "<div class='col-xs-12 col-md-4 item'>"+
-							  "<h4>"+ name +"</h4>" + 
-							  " <img src= " + img + " >" + 
-						      "<p> 演出單位 : " + performer + "</p>" +
-						      "<p> 活動日期 : " + date + " "+ startTime +"~" +endTime+ "</p>" +
-						      "<p> 活動地點 : " + place + "</p>" + 
-					      "</div> ";			 
-				var $art = $(art);
-				$('#artBox').append($art);			
-			}
-												
-		} else {
+  
+	function listData(found){								 						      						 				      
+		var	art = "<div class='col-xs-12 col-md-4 item'>"+
+					  "<h4>"+ found.PRGNAME +"</h4>" + 
+					  " <img src= " + found.IMAGE1 + " >" + 
+				      "<p> 演出單位 : " + found.PRGACT + "</p>" +
+				      "<p> 活動日期 : " + found.PRGDATE + " "+ found.PRGSTIME +"~" +found.PRGETIME+ "</p>" +
+				      "<p> 活動地點 : " + found.PRGPLACE + "</p>" + 
+			      "</div> ";			 
+		var $art = $(art);
+		$('#artBox').append($art);				
+    }
+    function showArtData(data,callback){
+    	var location = $('#location').val(),
+			ticket = $('#ticket').val(),
+			itemLocation = checkLocation(location),
+			itemTicket = checkTicket(ticket),
+			artData = [] ;
+		console.log(data);
+		$.each(data,function(i,item){
+			if(itemLocation === item.ORGNAME && itemTicket === item.PRGTICKET){				
+				callback(data[i]);
+				artData.push(data[i]);
+			} 
+		});
+		if(artData.length === 0){
 			$('#artBox').append("<p>沒有符合的資料!</p>");
 		}
-		
+		console.log(artData);
     }
-    $(window).scroll(function(){
-		var scrollValue = $(this).scrollTop(),
-			windowHeight = $(window).height()*0.5;
-		if(window.innerWidth > 992){
-			if (scrollValue > windowHeight) {
-				$('#up').fadeIn();
-			} else {
-				$('#up').fadeOut();
-			}
-		}
-	});
 
-    $('#up').click(function(){
-		$('html,body').animate({scrollTop:0}, 1000);
-	});
- 
 	$('#serch').on('click', function(e){
 		e.preventDefault();
+		$('#artBox').empty();
 		$.ajax({
 			url: "http://opendata.khcc.gov.tw/public/OD_art_program.ashx?SDate=2016/08/02&EDate=2017/02/02",
 			dataType: 'json',
 			method: "GET",
-			success : function(data){
-				var location = $('#location').val(),
-					ticket = $('#ticket').val(),
-					itemLocation = checkLocation(location),
-					itemTicket = checkTicket(ticket),
-					artData = [] ;
-				console.log(data);
-				$.each(data,function(i,item){
-					if(itemLocation === item.ORGNAME && itemTicket === item.PRGTICKET){
-						artData.push(data[i]);
-					}
-				})
-				console.log(artData);
-				$('#artBox').empty();
-				listData(artData);											
-			}
+			success: function(data){			
+				showArtData(data,listData);
+			}				
 		})
 	});	
 });
